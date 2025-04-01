fb17f8ba1565fc41dca678098502dc53d216cda3
Ngoc Hung <ngochungnguyen2000dhktyddn@gmail.com>
2025-04-01 19:08:37 +0700

    add img news

--- models/News.js
+++ models/News.js
@@ -4,6 +4,7 @@ const NewsSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    imageUrl: { type: String },
     author: {
       type: String,
       required: true,
