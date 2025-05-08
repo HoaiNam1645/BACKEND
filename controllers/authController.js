28cd070356318a71cdb96946ec36958c308e2c1a
admin <admin@MacBook-Pro.local>
2025-05-08 18:03:01 +0700

    login google

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const authService = require("../services/authService");
+const passport = require("passport");
 
 const register = async (req, res) => {
   const result = await authService.register(req.body);
@@ -10,4 +11,17 @@ const login = async (req, res) => {
   return res.status(result.code).json(result);
 };
 
-module.exports = { register, login };
+const googleCallback = async (req, res) => {
+  try {
+    const result = await authService.googleAuth(req.user)
+    return res.status(result.code).json(result);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Authentication failed",
+      error: error.message
+    });
+  }
+};
+
+module.exports = { register, login, googleCallback };
